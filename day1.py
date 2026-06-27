80d174439392a86197631fc48de169024c9532d8
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-27 18:56:02 +0530

    data types and operators

---
+++ day1.py
@@ -0,0 +1,54 @@
+
+# This is simple hello word
+
+# a=8
+# b=5
+# print(a+b)
+
+"""
+djvc
+dkjc
+csdjc
+sclkdcjk
+"""
+
+# print("Hello I am good")
+# print('Hello good father"s my""""" name is Neeraj')
+# print('Hello good father"s my \' name is Neeraj')
+# print('Hello good father\'s')
+
+# print("My name is Neeraj \nHello how are you")
+
+# print("My name ' is Neerajs Hello how are you")
+
+# print('My name " is Neerajs Hello \' how are you')
+
+# print('My name\t\thow are you') 
+
+
+# pranvikumar3254365i___=78
+# print(pranvikumar3254365i___)
+
+# name=67
+# name=45
+# NAME=78
+# print(name)
+# print(NAME)
+
+# int a=8
+
+# a=7
+# print(a)
+# print(type(a))
+
+# b=5.57
+# print(b)
+# print(type(b))
+
+# c="Rohan"
+# print(c)
+# print(type(c))
+
+# d=True
+# print(d)
+# print(type(d))
---
+++ day3.py
@@ -0,0 +1,99 @@
+# This is my simple line
+# print("Hello How are you")
+
+# kjdcv
+# md creditsvc dfx
+# vdf vc
+# kjhbkjhkjhk klklnkj
+
+# print("Hello my name \\n is pranvi \nhow are you")
+# print("Hello my \" \' \" name how are you")
+# print("Hello my\t\tname how are you")
+
+# var1=46
+# pranvikumari5646#$%^&*()(*&67=46
+# print(pranvikumari564667)
+
+# age=67
+# age=56
+# AGE=90
+# print(age)
+
+# a=True
+# print(a)
+# print(type(a))
+
+# a=6
+# print(a*a)
+
+# breadth=5
+# length=8
+# print("The area of rectangle is:",(length*breadth))
+
+# num1=5
+# num2=2.67
+# sum=int(num1+num2)
+
+# print(sum)
+# print(type(sum))
+
+# str1="Hello"
+# str2="Suman"
+# str1="5"
+# str2="8"
+# print(str1+str2)
+
+# name1="Rohan"
+# name2="Suman"
+# name3="Geeta"
+
+# print(name1,name2,name3)
+# print(name1+name2+name3)
+# print(name1,end=" ")
+# print(name2,end="-")
+# print(name3)
+
+# age=input("Enter the age: ")
+# print("My age is:",age)
+
+# num1=float(input("Enter the num1: "))
+# num2=float(input("Enter the num2: "))
+# print("The sum is:",(num1+num2))
+
+# num1=6
+# num2=3
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+
+
+# print(num1//num2)
+# print(num1%num2)
+# print(num1**num2)
+
+num1=6
+num2=7
+
+# print(num1>num2)
+# print(num1>=num2)
+# print(num1<num2)
+# print(num1<=num2)
+# print(num1==num2)
+# print(num1!=num2)
+
+
+# a=6
+# # a=a+5
+# a+=5
+# a*=3
+# print(a)
+
+# a=6
+# b=8
+# print(a>b and b<=a and b>=a)
+# print(a>b or b<=a or b>=a)
+# print(15>8 and 16<38 and 16>9)
+# print(15>8 or 16<8 or 16>9)
+# print(not(15>28))
