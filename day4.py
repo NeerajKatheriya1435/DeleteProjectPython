7aa84e885f33f84c9dd5d11e88cb3f1957f6ae2d
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-30 05:59:02 +0530

    if else and strings methods

---
+++ day4.py
@@ -0,0 +1,109 @@
+
+# name="Neeraj Katheriya"
+# name="Pranvi Bhardwaj"
+# print(name)
+# print(name[2])
+# print(name[5])
+
+# print(name[0:5])
+# print(name[3:5])
+# print(name[3:])
+# print(name[:5])
+# print(name[::3])
+
+# name="Pranvi Bhardwaj" \
+# "jkvjfd" \
+# "kjcdv" \
+# "kjvf"
+# print(name)
+# print(name[-2])
+# print(name[-8:-3])
+# print(name[3:])
+
+# name="""
+# hello gyus
+# how are you
+# i am fine
+# """
+
+# print(name)
+# name="Rekha"
+
+name="Rekha Kumari Singh"
+# print(name.upper())
+# print(name.lower())
+
+# name=input("Enter your name: ")
+# print(name)
+# print(name.strip("!"))
+
+# print(name.replace("Kumari","Singh"))
+# print(name.split(" "))
+
+# str1 = "hello world"
+# capStr1 = str1.capitalize()
+# print(capStr1)
+
+# str1 = "--------------------"
+# print(str1.center(70))
+# name="Rekha Kumari"
+# print(name.count("a"))
+
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("!!!&"))
+
+# name="Rekha Singh How are you"
+# print(name.find("Singh65"))
+
+# print("Important Line")
+
+# name="Rekha Singh How are you"
+# print(name.index("Singh"))
+
+# str1 = "WelcomeToTheConsole346547"
+# print(str1.isalnum())
+# str1 = "WelcomeToTheConsole"
+# print(str1.isalpha())
+
+# str1 = "We wish you a Merry Christmas"
+# print(str1.isprintable())
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.swapcase())
+
+# str1 = "hello palak how are you"
+# print(str1.title())
+
+# age=int(input("Enter your age: "))
+
+# if(age<=0):
+#     print("Invalid age")
+# elif(age>0 and age<=18):
+#     print("You are child can not drive")
+# elif(age>18 and age<=80):
+#     print("You can drive")
+# elif(age>80 and age<=120):
+#     print("You are overaged")
+# else:
+#     print("Inavlid age")
+
+
+
+# age=int(input("Enter your age: "))
+
+# if(age>18):
+#     print("You can drive the car")
+#     if(age==45):
+#         print("You are in special age")
+# else:
+#     print("You can not")
+
+# marks = 9
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 75:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# else:
+#     print("Fail")
