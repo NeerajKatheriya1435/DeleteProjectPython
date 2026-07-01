940d012a558101e9d652889e25b908c97b0e77d7
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-07-01 18:05:30 +0530

    List and tuples and strfmt

---
+++ day6.py
@@ -0,0 +1,74 @@
+
+# l1=[4,5,67,7,5,"Rohan",True,[4,5,6]]
+
+# pranvi=[4,5,7,7,5,3,7,9]
+
+# print(pranvi[4])
+# print(pranvi[2:8])
+# print(pranvi[2:])
+# print(pranvi[::2])
+# print(pranvi[-2])
+# print(pranvi[-4:-1])
+# print(l1)
+
+# lst1=[i for i in range(15) if i>10]
+# print(lst1)
+
+# colors = [4,8,6,9,3,9,6]
+# colors.append(90)
+# colors.append(89)
+# colors.append(34)
+# colors.sort()
+# colors.reverse()
+# print(colors)
+# print(colors.index(9))
+
+# print(colors.count(9))
+# newlist=colors.copy()
+# print(colors)
+# print(newlist)
+
+# colors.insert(3,567)
+# print(colors)
+
+# colors1 = [4,8,6,9,3,9,6]
+# colors2 = [5,8,90]
+# colors1.extend(colors2)
+# print(colors1)
+
+# t1=(4,5,6,7,5,"Rohan",True)
+
+# t1=(12,2,2002)
+# t1[2]=87687
+# print(t1)
+# t1=[12,2,2002]
+# t1[2]=87687
+# print(t1)
+
+# t1=(12,2,2002)
+# t1[2]=87687
+# print(t1)
+# print(t1[2])
+
+# list1=list(t1)
+# list1.insert(2,56778)
+# t1=tuple(list1)
+
+# print(t1)
+
+# t1=(12,2,2002,4,5,62,2,2)
+
+# for item in t1:
+#     print(item)
+
+# print(t1.count(2))
+# print(t1.index(4))
+
+name="Rohan"
+age=67
+
+# str1="My name is {1} and age is {0} dfgvdfv"
+# print(str1.format(name,age))
+# print(str1.format(age,name))
+
+# print(f"My {{name}} name is {name} and age is {age}")
